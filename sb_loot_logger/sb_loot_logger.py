ff778b9d2a7f092f68b6476abb597258d11d5f9c
ryaaahs <reily.maahs@gmail.com>
2024-03-31 19:54:12 -0400

    Fixed player item drop issue
    When a player would drop an item, it would come into the world as a loot item. Causing the system to log it. This update checks the memory addresses of the item and the chest before making a log entry.

--- sb_loot_logger/sb_loot_logger.py
+++ sb_loot_logger/sb_loot_logger.py
@@ -4,6 +4,7 @@ import util
 import time
 import json
 import ctypes
+import time
 
 from _remote import ffi, lib
 from manager import PluginBase
@@ -41,7 +42,11 @@ class Plugin(PluginBase):
         self.do_chest_log = True
         self.new_subworld = False
         self.is_home = True
-        self.current_looted_items = []
+        self.current_floor_looted_items = []
+        self.current_floor_chests = []
+        self.current_subworld_looted_items = []
+        self.current_floor = 0
+        self.current_zone = ""
         
         # Check to see if files exists before using them later
         if not os.path.exists(LOOT_LOGPATH):
@@ -70,6 +75,9 @@ class Plugin(PluginBase):
         cwprops = cw.asWorld.props
         zone = util.getstr(cw.asWorld.props.zone) or util.getstr(cw.asWorld.props.music)
         floor = cwprops.floor;
+        
+        # Log floor loot
+        logFloorLoot(self, floor, zone)
 
         # If we're not in the ignored zones 
         if zone not in IGNORED_ZONE:
@@ -89,10 +97,12 @@ class Plugin(PluginBase):
                         logging.info("Finished writing to Loot file")
                 self.new_subworld = True
                 self.is_home = False
+                self.current_floor = floor 
+                self.current_zone = zone
 
             for obj in util.worldobjects(cw.mySubWorld.asNativeSubWorld):
-                if util.getClassName(obj) == "Loot":
-                    logLoot(self, obj)
+                if util.getClassName(obj) == "Loot" or util.getClassName(obj) == "Chest":
+                    logLoot(self, obj, util.getClassName(obj))
         else: 
             if not self.is_home: 
                 with open(LOOT_LOGPATH, 'a') as file:
@@ -102,7 +112,7 @@ class Plugin(PluginBase):
                     if DEBUG_MODE:
                         logging.info("Finished writing to Loot file")
 
-                self.current_looted_items = []
+                self.current_subworld_looted_items = []
                 self.is_home = True
                 self.new_subworld = False
 
@@ -118,27 +128,54 @@ def reFieldToList(rf, itemtype=None):
             lst[e] = ffi.cast(itemtype, lst[e])
     return lst
 
-def logBossZoneString(currentZone): 
-        logString = currentZone
-        if not logString.endswith("boss"): 
-            logString += "_mid_boss"
+def logMidBossZoneString(current_zone, current_floor): 
+    zone_string = current_zone
+    
+    if (current_zone in MID_BOSS and MID_BOSS[current_zone] == current_floor):
+        zone_string += "_mid_boss"
+    else: 
+        # If we don't have it, return the original string
+        return current_zone
 
-        return logString;
+    return zone_string;
 
-def logLoot(self, obj):
-    loot = ffi.cast('struct Loot *', obj)
+def logLoot(self, obj, class_name):
+    cw = self.refs.ClientWorld
 
-    if str(loot) in self.current_looted_items:
-        return
-    else:
-        logging.info("Loot Found!")
-        self.current_looted_items.append(str(loot));
+    if (class_name == "Loot"):
+        loot = ffi.cast('struct Loot *', obj)
+        item_prop = loot.itemProps
+        item_desc = loot.itemDesc
+        item_type = slotType(item_desc.slot)
 
     item_info = {}
-    item_type = ""
-    item_prop = loot.itemProps
-    item_desc = loot.itemDesc
     
+    if obj.objId in self.current_floor_looted_items:
+        return
+    else:
+        logging.info("New Loot Found!")
+
+        if class_name == "Loot" and item_type == "Miscellaneous":
+            logging.info(util.getstr(loot.itemDesc.name) + ": " + str(obj.objId))
+            self.current_floor_looted_items.append(obj.objId)
+        else:
+            # We need to confirm if the other types of Loot are dropped from a chest
+            if class_name == "Chest":
+                chest = ffi.cast('struct Chest *', obj)
+                
+                # Wait till all items come out of the chest before running the logic
+                if obj.objId not in self.current_floor_chests and chest.pos == util.veclen(chest.angles):
+                    for item in  reFieldToList(chest.items, 'struct ItemProperties *'):
+                        for loot_obj in util.worldobjects(cw.mySubWorld.asNativeSubWorld):
+                            # Catch any ids that made it in already (Mostly misc)
+                            if util.getClassName(loot_obj) == "Loot" and loot_obj.objId not in self.current_floor_looted_items:
+                                chest_loot = ffi.cast('struct Loot *', loot_obj)
+                                # Compare memory addresses to see if they are the same
+                                if str(item.classptr) == str(chest_loot.itemProps.classptr):
+                                    # We found a match
+                                    self.current_floor_looted_items.append(loot_obj.objId)
+                    self.current_floor_chests.append(obj.objId);
+
     if DEBUG_MODE:
         logging.info("----")
         logging.info("ItemProperties")
@@ -156,20 +193,6 @@ def logLoot(self, obj):
         logging.info(" - itemclass:" + str(item_desc.itemclass))
         logging.info(" - itemsubclass:" + str(item_desc.itemsubclass))
         logging.info(" - slot:" + str(item_desc.slot))
-
-    # If slot is zero, we know that the drop is a miscellaneous item
-    if item_desc.slot == MISC: 
-        item_type = "Miscellaneous"
-    elif item_desc.slot == SLOT_MAIN:
-        item_type = "Main"
-    elif item_desc.slot == SLOT_SECOND:
-        item_type = "Second"
-    elif item_desc.slot == SLOT_SPECIAL:
-        item_type = "Special"
-    elif item_desc.slot == SLOT_MOBILITY:
-        item_type = "Mobility"
-    elif item_desc.slot == SLOT_BODY:
-        item_type = "Body"
 
     if item_prop.statboosts != ffi.NULL and item_desc.slot != MISC: 
         for boost in reFieldToList(item_prop.statboosts, 'struct StatBoost *'):
@@ -195,6 +218,49 @@ def logLoot(self, obj):
 def writeToDebugFile(test_string): 
         with open(DEBUG_FILE_LOGPATH, 'a') as file:
             file.write(test_string)
-            file.write('\n')
             if DEBUG_MODE:
                 logging.info("Finished writing to Debug file")
+
+def slotType(slot_value):
+    # If slot is zero, we know that the drop is a miscellaneous item
+    if slot_value == MISC: 
+        return "Miscellaneous"
+    elif slot_value == SLOT_MAIN:
+        return "Main"
+    elif slot_value == SLOT_SECOND:
+        return "Second"
+    elif slot_value == SLOT_SPECIAL:
+        return "Special"
+    elif slot_value == SLOT_MOBILITY:
+        return "Mobility"
+    elif slot_value == SLOT_BODY:
+        return "Body"
+    
+    return ""
+
+
+def logFloorLoot(self, floor, zone):
+    if (zone not in IGNORED_ZONE and self.current_floor != floor) or (zone in IGNORED_ZONE and not self.is_home) :
+        logging.info("Floor Change!")
+
+        log_data = {
+            "date": time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()), 
+            "location": logMidBossZoneString(self.current_zone, self.current_floor),
+            "floor": self.current_floor,
+            "floor_loot": self.current_floor_looted_items
+        }
+
+        with open(LOOT_LOGPATH, 'a') as file:
+                file.write('Floor: ' + str(self.current_floor))
+                file.write('\n')
+                json.dump(log_data, file)
+                file.write('\n')
+
+                if DEBUG_MODE:
+                    logging.info("Finished writing to Loot file")
+
+        self.current_floor = floor
+        self.current_zone = zone
+        self.current_subworld_looted_items.append(self.current_floor_looted_items)
+        self.current_floor_looted_items = []
+        self.current_floor_chests = []
