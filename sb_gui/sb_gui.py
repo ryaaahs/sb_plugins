309ea0a491a0b907f3ab09378aa6553201f2e860
ryaaahs <reily.maahs@gmail.com>
2024-09-30 20:37:01 -0400

    Update gui logic to work as an import

--- sb_gui/sb_gui.py
+++ sb_gui/sb_gui.py
@@ -18,19 +18,49 @@ class Graphic:  # make subclass of PluginBase?
         self.y = -1
         self.w = 0
         self.h = 0
+        self.draw_top_border = False
+        self.draw_bottom_border = False
+        self.draw_right_border = False
+        self.draw_left_border = False
         self.fill_rect = "0xff2b2b2b"
         self.fill_rect_blend = lib.BLENDMODE_BLEND
         self.draw_rect = "0xffa0a0a0"
         self.draw_rect_blend = lib.BLENDMODE_BLEND
 
+    # Toggle the border display
+    def drawTopBorder(self):
+        self.draw_top_border ^= True
+
+    def drawBottomBorder(self):
+        self.draw_bottom_border ^= True
+
+    def drawRightBorder(self):
+        self.draw_right_border ^= True
+
+    def drawLeftBorder(self):
+        self.draw_left_border ^= True
+
+    def drawBorder(self): 
+        self.drawTopBorder()
+        self.drawBottomBorder()
+        self.drawRightBorder()
+        self.drawLeftBorder()
+
     def draw(self):  # draws background
         cw = self.refs.canvasW_[0]
         ch = self.refs.canvasH_[0]
+        
         self.refs.canvasW_[0] = self.refs.windowW
         self.refs.canvasH_[0] = self.refs.windowH
 
         self.refs.XDL_FillRect(self.x, self.y, self.w, self.h, int(self.fill_rect, 16), self.fill_rect_blend)
-        self.refs.XDL_DrawRect(self.x, self.y, self.w, self.h, int(self.draw_rect, 16), self.draw_rect_blend)
+        
+        x = self.x if self.draw_top_border == True else 0
+        y = self.y if self.draw_bottom_border == True else 0
+        w = self.w if self.draw_right_border == True else 0
+        h = self.h if self.draw_left_border == True else 0
+
+        self.refs.XDL_DrawRect(x, y, w, h, int(self.draw_rect, 16), self.draw_rect_blend)
 
         self.refs.canvasW_[0] = cw
         self.refs.canvasH_[0] = ch
@@ -41,26 +71,12 @@ class GraphicWindow(Graphic):
         self.panel_spacing_w = 3
         self.panel_spacing_h = 5
         self.text_position = 0
+        self.base_window_y = 0
         self.panels = []
 
     def addPanelGroup(self, panel_group): self.panels.append(panel_group)
     def addLabel(self, text=None, position=1): self.panels.append(GraphicPanelLabel(self.refs, self, self, text, position))
     def addPanelDivider(self, text=None): self.panels.append(GraphicPanelDivider(self.refs, self, self))
-
-    # def reset(self, x, y):
-    #     # Calculate window and panel values
-    #     self.w = 0
-    #     self.h = self.panel_spacing_h
-        
-    #     for panel in self.panels:
-    #         panel.x = self.panel_spacing_w
-    #         panel.y = self.h
-    #         self.w = max(self.w, panel.w + 2 * self.panel_spacing_w)
-    #         self.h += panel.h + 1
-    #     self.h += self.panel_spacing_h
-
-    #     self.x = min(x, self.refs.windowW - self.w)
-    #     self.y = y if y + self.h < self.refs.windowH else max(y - self.h, 0)
 
     def defineWindow(self, panels):
         for panel in panels:
@@ -70,15 +86,12 @@ class GraphicWindow(Graphic):
                 self.w = max(self.w, panel.w + 2 * self.panel_spacing_w)
                 self.h += panel.h 
 
-    def renderNestedPanels(self, panels, x, y):
+    def renderNestedPanels(self, panels):
         for panel in panels:
             if isinstance(panel, PanelGroup):    
                 # levels = 1
-                # logging.info("a: " + str(self.text_position))
-                self.text_position += panel.reset(x, self.text_position, self.w, 1)
-                # logging.info("b: " + str(self.text_position))
-                self.renderNestedPanels(panel.panels, x, y)
-                #Render all the blocks with full content
+                self.text_position += panel.reset(self.x, self.text_position, self.w, 1, self.base_window_y)
+                self.renderNestedPanels(panel.panels)
             else:
                 if not isinstance(panel.parent, PanelGroup):
                     panel.x = self.panel_spacing_w 
@@ -86,22 +99,19 @@ class GraphicWindow(Graphic):
 
                     self.text_position += panel.h
 
-                    self.x = min(x, self.refs.windowW - self.w)
+                    self.x = min(self.x, self.refs.windowW - self.w)
 
-                    # logging.info(self.refs.windowH)
-
-                    self.y = y if y + self.h < self.refs.windowH else max(y - self.h, 0)
+                    self.y = self.y if self.y + self.h < self.refs.windowH else max(self.y - self.h, 0)
                 
     def reset(self, x, y):
+        self.x = x
+        self.y = y
+        self.base_window_y = y
         # Calculate window and panel values
         self.defineWindow(self.panels)
-        self.renderNestedPanels(self.panels, x, y)
-
-        self.h += self.panel_spacing_h
+        self.renderNestedPanels(self.panels)
 
     def draw(self):
-        # pass
-        # logging.info(self.y)
         super().draw()
         for panel in self.panels:
             panel.draw()
@@ -112,12 +122,12 @@ class PanelGroup(Graphic):
         self.tag = tag
         self.window = window
         self.parent = None
-        logging.info(window.fill_rect)
         self.set_fill_rect = ""
         self.set_draw_rect = ""
         self.panel_spacing_w = 3
         self.panel_spacing_h = 5
-        self.text_position = 0;
+        self.text_position = 0
+        self.base_window_y = 0
         self.levels = 0
         self.panels = []
 
@@ -135,7 +145,7 @@ class PanelGroup(Graphic):
             if isinstance(panel, PanelGroup):
                 self.defineWindow(panel.panels)
             else:
-                # self.w = max(self.w, panel.w + 2 * self.panel_spacing_w)
+                self.w = (max(self.w, panel.w + 2 * self.panel_spacing_w) - 1)
                 self.h += panel.h 
 
     def setDisplayColour(self):
@@ -153,47 +163,38 @@ class PanelGroup(Graphic):
 
 
     def renderNestedPanels(self, panels, x, y):
-       
         for panel in panels:
             if isinstance(panel, PanelGroup):
-                # logging.info("a" + self.tag + " " + str(self.levels))
-                # logging.info("b" + self.tag + " " + str(self.levels))
-                # logging.info("a: " + str(self.text_position))
-                self.text_position += panel.reset(x, self.text_position, self.w, 1)
-                # logging.info("b: " + str(self.text_position))
-            else:
+                self.text_position += panel.reset(x, self.text_position, self.w, 1, self.base_window_y)
                 
-                # logging.info(self.levels)
-                # logging.info("a" + self.tag + " " + str(self.levels))
+            else:
                 panel.x = self.panel_spacing_w + (self.levels * 5)
-                panel.y = self.text_position 
+                panel.y = self.text_position
 
                 self.h += panel.h
                 self.text_position += panel.h
 
-    def reset(self, x, y, width, levels):
-        # Calculate window and panel values
-        # logging.info(self.tag)
-        # logging.info(y)
+                self.x = min(x, self.refs.windowW - self.w)
+                self.y = (y + self.base_window_y) if y + self.h < self.refs.windowH else max(y - self.h, 0)
+                
+                
+
+    def reset(self, x, y, width, levels, base_window_y):
+        # Grab parent colours if we have none
         self.setDisplayColour()
 
+        # Calculate window and panel values
+        self.base_window_y = base_window_y
         self.levels += levels
 
         self.h = 0
         self.text_position = y
         self.w = width
-        
+
         self.renderNestedPanels(self.panels, x, y)
-
-        self.x = min(x, self.refs.windowW - self.w)
-        self.y = self.window.y + y if y + self.h < self.refs.windowH else max(y - self.h, 0)
-
         return self.h
 
     def draw(self):
-        pass
-        # self.reset(20, 20)
-        # logging.info(self.y)
         super().draw()
         for panel in self.panels:
             panel.draw()
@@ -247,99 +248,60 @@ class GraphicPanelLabel(GraphicPanel):
             self.text_ready = False
 
     def draw(self):
-        # logging.info(self.y)
-        # logging.info(self.window.x)
         if self.text_ready:
             self.text_obj.draw(self.window.x + self.x + self.text_spacing_x, self.window.y + self.y + self.text_spacing_y)
 
-class Plugin(PluginBase):
-    def onPresent(self):
-        main_window = GraphicWindow(self.refs)
-        main_window_two = GraphicWindow(self.refs)
-        main_window_three = GraphicWindow(self.refs)
 
-        main_window.addLabel("Test333333")
-        main_window.addPanelDivider()
+# For testing within the Plugin folder of SBPE
+# Normal use would be pulling in the file from the main directory of the SBPE
+# class Plugin(PluginBase):
+#     def onPresent(self):
+        # pass
+        # main_window = GraphicWindow(self.refs)
+        # main_window.drawBorder()
 
-        panel_group_one = PanelGroup(self.refs, main_window, "pg1")
-        panel_group_one.changeFillRectColour("0xffb27070")
-        panel_group_one.changeDrawRectColour("0xffb27070")
-        panel_group_one.addLabel("A")
-        panel_group_one.addLabel("A")
-        panel_group_one.addLabel("A")
-        panel_group_one.addLabel("A")
-        panel_group_one.addLabel("A")
+        # panel_group_one = PanelGroup(self.refs, main_window, "pg1")
+        # panel_group_one.changeFillRectColour("0xffb27070")
+        # panel_group_one.changeDrawRectColour("0xffb27070")
+        # panel_group_one.drawBorder()
+        # panel_group_one.addLabel("e")
+        # panel_group_one.addLabel("A")
+        # panel_group_one.addLabel("A")
+        # panel_group_one.addLabel("A")
+        # panel_group_one.addLabel("A")
 
-        panel_group_two = PanelGroup(self.refs, main_window, "pg2")
-        panel_group_two.changeFillRectColour("0xffbedbb1")
-        panel_group_two.addLabel("B")
-        panel_group_two.addLabel("B")
-        panel_group_two.addLabel("B")
-        panel_group_two.addLabel("B")
-        panel_group_two.addLabel("B")
-        panel_group_two.addPanelDivider()
-
-        panel_group_three = PanelGroup(self.refs, main_window, "pg3")
-        panel_group_three.changeFillRectColour("0xff7d7495")
-        panel_group_three.addLabel("Test1222222222222222222222222222222222222222111111111111111111111", 1)
-
-        # panel_group_three.fill_rect_blend = lib.BLENDMODE_MOD
-
-        panel_group_four = PanelGroup(self.refs, main_window, "pg4")
-        panel_group_four.changeFillRectColour("0xff21527f")
-        panel_group_four.addLabel("C")
-        panel_group_four.addPanelDivider()
-
-        panel_group_five = PanelGroup(self.refs, main_window, "pg5")
-        panel_group_five.changeFillRectColour("0xff7d7495")
-        panel_group_five.addLabel("D")
-
-        panel_group_six = PanelGroup(self.refs, main_window, "pg6")
+        # panel_group_six = PanelGroup(self.refs, main_window, "pg6")
         # panel_group_six.changeFillRectColour("0xff7d7495")
-        panel_group_six.addLabel("E", 1)
-        panel_group_six.addLabel("E", 1)
-        panel_group_six.addLabel("E", 1)
-        panel_group_six.addLabel("E", 1)
-        panel_group_six.addLabel("E", 1)
-        panel_group_six.addLabel("E", 1)
+        # panel_group_six.drawBorder()
+        # panel_group_six.addLabel("E", 1)
+        # panel_group_six.addLabel("E", 1)
+        # panel_group_six.addLabel("E", 1)
+        # panel_group_six.addLabel("E", 1)
+        # panel_group_six.addLabel("E", 1)
+        # panel_group_six.addLabel("E", 1)
 
-
-        panel_group_one.addPanelGroup(panel_group_six)
-        panel_group_two.addPanelGroup(panel_group_three)
-        panel_group_three.addPanelGroup(panel_group_four)
-        panel_group_four.addPanelGroup(panel_group_five)
+        # panel_group_two = PanelGroup(self.refs, main_window, "pg2")
         
-            
-        main_window.addPanelGroup(panel_group_one)
-        main_window.addPanelGroup(panel_group_two)
-        
+        # panel_group_two.changeFillRectColour("0xffbedbb1")
+        # panel_group_two.drawBorder()
+        # panel_group_two.addLabel("B")
+        # panel_group_two.addLabel("B")
+        # panel_group_two.addLabel("B")
+        # panel_group_two.addLabel("B")
+        # panel_group_two.addLabel("B")
+        # panel_group_two.addPanelDivider()
 
-        main_window.addLabel("Test333333")
-        main_window.addPanelDivider()
+        # panel_group_one.addPanelGroup(panel_group_six)
 
-        main_window.addLabel("Test333333")
-        main_window.addPanelDivider()
-        
-        # main_window.addLabel("Test1")
-        # main_window.addLabel("Test2", 2)
-        # main_window.addLabel("Test1", 2)
-        # main_window.addLabel("Test2", 2)
+        # main_window.addPanelGroup(panel_group_one)
+        # main_window.addPanelGroup(panel_group_two)
+
+        # main_window.addLabel("Test333333")
         # main_window.addPanelDivider()
-        # main_window.addLabel("Test1")
-        # main_window.addLabel("Test2")
-        main_window.reset(20, 20)
 
-        main_window_two.addLabel("Test2")
-        main_window_two.addPanelDivider()
-        main_window_two.addLabel("Test2")
-        main_window_two.addLabel("Test1")
-        main_window_two.addLabel("Test2")
-        main_window_two.addPanelDivider()
-        main_window_two.reset(25 + main_window.w, 20)
-
-        main_window_three.addLabel("Test3")
-        main_window_three.reset(30 + main_window.w + main_window_two.w, 20)
-      
-        main_window.draw()
-        main_window_two.draw()
-        main_window_three.draw()
+        # main_window.addLabel("Test333333")
+        # main_window.addPanelDivider()
+        
+        # main_window.reset(20, 20)
+        
+        # main_window.draw()
