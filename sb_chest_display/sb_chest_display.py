95099a7679f34815e54322d957eae72fcc4817ee
ryaaahs <reily.maahs@gmail.com>
2024-10-26 03:39:42 -0400

    Fix HR conditional check

--- sb_chest_display/sb_chest_display.py
+++ sb_chest_display/sb_chest_display.py
@@ -28,6 +28,310 @@ ITEM_MODS = [
 # Zones that we're ignoring
 IGNORED_ZONE = ("home", "lobby")
 
+class Graphic:  # make subclass of PluginBase?
+    def __init__(self, refs):
+        self.refs = refs
+        self.x = -1
+        self.y = -1
+        self.w = 0
+        self.h = 0
+        self.draw_top_border = False
+        self.draw_bottom_border = False
+        self.draw_right_border = False
+        self.draw_left_border = False
+        self.fill_rect = 0xFF2B2B2B
+        self.fill_rect_blend = lib.BLENDMODE_BLEND
+        self.draw_rect = 0xFFA0A0A0
+        self.draw_rect_blend = lib.BLENDMODE_BLEND
+
+    # Toggle the border display
+    def drawTopBorder(self):
+        self.draw_top_border ^= True
+
+    def drawBottomBorder(self):
+        self.draw_bottom_border ^= True
+
+    def drawRightBorder(self):
+        self.draw_right_border ^= True
+
+    def drawLeftBorder(self):
+        self.draw_left_border ^= True
+
+    def drawBorder(self):
+        self.drawTopBorder()
+        self.drawBottomBorder()
+        self.drawRightBorder()
+        self.drawLeftBorder()
+
+    def draw(self):  # draws background
+        cw = self.refs.canvasW_[0]
+        ch = self.refs.canvasH_[0]
+
+        self.refs.canvasW_[0] = self.refs.windowW
+        self.refs.canvasH_[0] = self.refs.windowH
+
+        if type(self.fill_rect) == "str":
+            self.fill_rect = int(str(fill_rect), 16)
+        if type(self.draw_rect) == "str":
+            self.draw_rect = int(str(draw_rect), 16)
+
+        self.refs.XDL_FillRect(
+            self.x, self.y, self.w, self.h, self.fill_rect, self.fill_rect_blend
+        )
+
+        x = self.x if self.draw_top_border == True else 0
+        y = self.y if self.draw_bottom_border == True else 0
+        w = self.w if self.draw_right_border == True else 0
+        h = self.h if self.draw_left_border == True else 0
+
+        self.refs.XDL_DrawRect(x, y, w, h, self.draw_rect, self.fill_rect_blend)
+
+        self.refs.canvasW_[0] = cw
+        self.refs.canvasH_[0] = ch
+
+
+class GraphicWindow(Graphic):
+    def __init__(self, refs):
+        super().__init__(refs)
+        self.panel_spacing_w = 3
+        self.panel_spacing_h = 20
+        self.text_position = 0
+        self.base_window_y = 0
+        self.panels = []
+        self.panel_is_first_item = True
+
+    def addPanelGroup(self, panel_group):
+        self.panels.append(panel_group)
+
+    def addLabel(self, text=None, position=1, colour=0xFFFFFF):
+        self.panels.append(
+            GraphicPanelLabel(self.refs, self, self, text, position, colour)
+        )
+
+    def addPanelDivider(self, text=None):
+        self.panels.append(GraphicPanelDivider(self.refs, self, self))
+
+    def defineWindow(self, panels, in_child):
+        if not in_child:
+            self.w = 0
+            self.h = 0
+
+        for panel in panels:
+            if isinstance(panel, PanelGroup):
+                self.defineWindow(panel.panels, True)
+            else:
+                self.w = max(self.w, panel.w + 2 * self.panel_spacing_w)
+                self.h += panel.h
+
+    def renderNestedPanels(self, panels):
+        for panel in panels:
+            if isinstance(panel, PanelGroup):
+                # levels = 1
+                if self.panel_is_first_item:
+                    self.text_position += panel.reset(
+                        self.x, self.text_position + 1, self.w, 1, self.base_window_y
+                    )
+                    self.panel_is_first_item = False
+                else:
+                    self.text_position += panel.reset(
+                        self.x, self.text_position, self.w, 1, self.base_window_y
+                    )
+                self.renderNestedPanels(panel.panels)
+            else:
+                if not isinstance(panel.parent, PanelGroup):
+                    panel.x = self.panel_spacing_w
+                    panel.y = self.text_position
+
+                    self.text_position += panel.h 
+
+                    self.x = min(self.x, self.refs.windowW - self.w)
+                    self.y = (
+                        self.y
+                        if self.y + self.h < self.refs.windowH
+                        else max(self.y - self.h, 0)
+                    )
+            self.panel_is_first_item = False
+        
+    def reset(self, x, y):
+        self.text_position = 0
+        self.x = x
+        self.y = y
+        self.base_window_y = y
+        # Calculate window and panel values
+        # self.defineWindow(self.panels)
+        self.renderNestedPanels(self.panels)
+
+    def draw(self):
+        super().draw()
+        for panel in self.panels:
+            panel.draw()
+
+
+class PanelGroup(Graphic):
+    def __init__(self, refs, window, tag=None):
+        super().__init__(refs)
+        self.tag = tag
+        self.window = window
+        self.parent = None
+        self.set_fill_rect = ""
+        self.set_draw_rect = ""
+        self.panel_spacing_w = 3
+        self.panel_spacing_h = 20
+        self.text_position = 0
+        self.base_window_y = 0
+        self.levels = 0
+        self.panels = []
+
+    def addPanelGroup(self, panel_group):
+        self.panels.append(panel_group)
+        panel_group.parent = self
+
+    def addLabel(self, text=None, position=1, colour=0xFFFFFF):
+        self.panels.append(
+            GraphicPanelLabel(self.refs, self.window, self, text, position, colour)
+        )
+
+    def addPanelDivider(self, text=None):
+        self.panels.append(GraphicPanelDivider(self.refs, self.window, self))
+
+    def changeFillRectColour(self, hexcode):
+        self.set_fill_rect = hexcode
+
+    def changeDrawRectColour(self, hexcode):
+        self.set_draw_rect = hexcode
+
+    def defineWindow(self, panels):
+        for panel in panels:
+            if isinstance(panel, PanelGroup):
+                self.defineWindow(panel.panels)
+            else:
+                self.w = max(self.w, panel.w + 2 * self.panel_spacing_w)
+                self.h += panel.h
+
+    def setDisplayColour(self):
+        if self.set_fill_rect == "" and self.parent != None:
+            if self.parent.set_fill_rect != "":
+                self.fill_rect = self.parent.set_fill_rect
+        elif self.set_fill_rect != "":
+            self.fill_rect = self.set_fill_rect
+
+        if self.set_draw_rect == "" and self.parent != None:
+            if self.parent.set_draw_rect != "":
+                self.draw_rect = self.parent.set_draw_rect
+        elif self.set_draw_rect != "":
+            self.draw_rect = self.set_draw_rect
+
+    def renderNestedPanels(self, panels, x, y):
+        for panel in panels:
+            if isinstance(panel, PanelGroup):
+                self.text_position += panel.reset(
+                    x, self.text_position + self.panel_spacing_h, self.w, 1, self.base_window_y
+                )
+            else:
+                panel.x = self.panel_spacing_w + (self.levels * 5)
+                panel.y = self.text_position
+
+                self.text_position += panel.h 
+
+                self.x = min(x, self.refs.windowW - self.w)
+                self.y = (
+                    (y + self.base_window_y)
+                    if y + self.h < self.refs.windowH
+                    else max(y - self.h, 0)
+                )
+
+    def reset(self, x, y, width, levels, base_window_y):
+        # Grab parent colours if we have none
+        self.setDisplayColour()
+
+        # Calculate window and panel values
+        self.base_window_y = base_window_y
+        self.levels += levels
+
+        self.text_position = y
+        
+        self.defineWindow(self.panels)
+        self.w = self.w - 1
+        self.h = self.h - 2
+        
+        self.renderNestedPanels(self.panels, x, y)
+
+        return self.h
+
+    def draw(self):
+        super().draw()
+        for panel in self.panels:
+            panel.draw()
+
+
+class GraphicPanel(Graphic):  # TODO: get refs from window
+    def __init__(self, refs, window):
+        super().__init__(refs)
+
+        self.window = window
+
+    def draw(self):
+        pass
+
+
+class GraphicPanelDivider(GraphicPanel):
+    def __init__(self, refs, window, parent):
+        super().__init__(refs, window)
+        self.h = 4
+        self.parent = parent
+
+    def draw(self):
+        cw = self.refs.canvasW_[0]
+        ch = self.refs.canvasH_[0]
+        self.refs.canvasW_[0] = self.refs.windowW
+        self.refs.canvasH_[0] = self.refs.windowH
+        # XDL_DrawLine(int x0, int y0, int x1, int y1, XDL_Color color, enum BlendMode blendMode)
+        self.refs.XDL_DrawLine(
+            self.window.x + 11,
+            self.window.y + self.y,
+            self.window.x + self.window.w - 11,
+            self.window.y + self.y,
+            0xFF808080,
+            lib.BLENDMODE_BLEND,
+        )
+        self.refs.canvasW_[0] = cw
+        self.refs.canvasH_[0] = ch
+
+
+class GraphicPanelLabel(GraphicPanel):
+    def __init__(self, refs, window, parent, text=None, position=1, colour=0xFFFFFF):
+        super().__init__(refs, window)
+        self.h = 22
+        self.position = position
+        self.parent = parent
+        self.text_spacing_x = 10 * position
+        self.label_spacing_x = 32
+        self.text_spacing_y = 5
+        self.colour = colour
+        self.text_obj = util.PlainText(
+            size=13, outlineSize=0
+        )  # Size should be 12 but real font size is set to 10
+        self.text_obj.color = self.colour
+        self.text_ready = False
+        if text != None:
+            self.setText(text)
+
+    def setText(self, string):
+        if string:
+            self.text_obj.text = string
+            self.text_obj.updateTexture()
+            self.w = self.text_obj.w + 2 * self.label_spacing_x
+            self.text_ready = True
+        else:
+            self.w = 2 * self.label_spacing_x
+            self.text_ready = False
+
+    def draw(self):
+        if self.text_ready:
+            self.text_obj.draw(
+                self.window.x + self.x + self.text_spacing_x,
+                self.window.y + self.y + self.text_spacing_y,
+            )
 
 class Plugin(PluginBase):
     def onInit(self, inputs=None):
@@ -246,12 +550,11 @@ class Plugin(PluginBase):
                             text_element_index += 1
 
                             displays[display_index].addLabel(element["item"]["text"])
-                            if (chest_length < max_item_display) and (chest_length != text_element_index): 
-                                displays[display_index].addPanelDivider()
-                            # First check to see if we are not on the max display
-                            # Second, check to see if we are at the end of the display dict list
-                            elif (max_item_display != text_element_index) and index != len(self.display_dict[game_object.objId]) - 1: 
-                                displays[display_index].addPanelDivider()
+                            if (text_element_index < max_item_display and index != len(self.display_dict[game_object.objId]) - 1): 
+                                # First check to see if we are not on the max display
+                                # Second, check to see if we are at the end of the display dict list
+                                displays[display_index].addPanelDivider() 
+                            
                             
                         else:
                             panel_group_number += 1
@@ -272,12 +575,10 @@ class Plugin(PluginBase):
                                 panel_group.addLabel(
                                     boost_elements["text"], 1, boost_elements["colour"]
                                 )   
-                            if (chest_length < max_item_display) and (chest_length != text_element_index): 
-                                panel_group.addPanelDivider()
                             # First check to see if we are not on the max display
                             # Second, check to see if we overflow on max display
                             # Third, check to see if we are at the end of the display dict list
-                            elif (max_item_display != text_element_index) and (text_element_index < max_item_display) and index != len(self.display_dict[game_object.objId]) - 1: 
+                            if (text_element_index < max_item_display) and index != len(self.display_dict[game_object.objId]) - 1: 
                                 panel_group.addPanelDivider()
 
                             displays[display_index].addPanelGroup(panel_group)
